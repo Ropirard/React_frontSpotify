208d0ef055bbe228337aef3732e0efc3958fb7ea
ES_PREPA_DEV <pirardrobin745@gmail.com>
2026-02-17 16:51:17 +0100

    Create online screens n add them to OnlineRouter

--- src/router/OnlineRouter.jsx
+++ src/router/OnlineRouter.jsx
@@ -2,6 +2,11 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import ErrorPage from "../screens/ErrorScreens/ErrorPage";
 import Home from "../screens/OnlineScreens/Home";
+import Detail from "../screens/OnlineScreens/Detail";
+import Library from "../screens/OnlineScreens/Library";
+import Playlist from "../screens/OnlineScreens/Playlist";
+import { Search } from "../screens/OnlineScreens/Search";
+import Wishlist from "../screens/OnlineScreens/Wishlist";
 
 const OnlineRouter = createBrowserRouter([
   {
@@ -11,6 +16,26 @@ const OnlineRouter = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/detail/:id",
+        element: <Detail />,
+      },
+      {
+        path: "/library",
+        element: <Library />,
+      },
+      {
+        path: "/playlist",
+        element: <Playlist />,
+      },
+      {
+        path: "/search",
+        element: <Search />,
+      },
+      {
+        path: "/wishlist",
+        element: <Wishlist />,
       },
     ],
   },
